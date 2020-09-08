1f39dd17abb3397f31e0e2be526c94bc2a25863d
likatare <55482170+likatare@users.noreply.github.com>
2020-09-08 10:22:29 +0200

    Add project files.

---
+++ Lab2/CRUD.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="CRUD" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CRUD.aspx.cs" Inherits="Lab2.CRUD" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2>
+    <h3>Your CRUD page.</h3>
+
+
+
+    <asp:GridView ID="GridView1" runat="server" Width="100%" CssClass="table table-striped table-bordered table-hover"
+        DataKeyNames="DatabaseLogId" AutoGenerateColumns="false" EmptyDataText="There are no data records to display"
+        Pagesize="10" AllowPaging="true" OnPageIndexChanging="OnPageIndexChanging"
+        PagerSettings-Position="TopAndBottom" PagerSettings-Mode="NumericFirstLast"
+        OnRowEditing="GridView1_RowEditing"
+        OnRowCancelingEdit="GridView1_RowCancelingEdit"
+        OnRowUpdating="GridView1_RowUpdating"
+        OnRowDeleting="GridView1_RowDeleting">
+        
+        <Columns>
+            <asp:BoundField DataField="PostTime" HeaderText="Post timer" SortExpression="PostTime" />
+            <asp:BoundField DataField="Event" HeaderText="Event" SortExpression="Event" />
+            <asp:CommandField ShowEditButton="true" />
+            <asp:CommandField ShowDeleteButton="true" />
+        </Columns>
+
+    </asp:GridView>
+
+</asp:Content>
---
+++ Lab2/CRUD.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab2
+{
+    public partial class CRUD : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("~/");
+            }
+
+            if (!Page.IsPostBack)
+            {
+                BindData();
+            }
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            var db = new AdventureWorks2019Entities();
+            int id = int.Parse(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+
+            TextBox textPostTime = (TextBox)(row.Cells[0].Controls[0]);
+            TextBox textEvent = (TextBox)(row.Cells[1].Controls[0]);
+
+            var item = db.DatabaseLog.Where(x => x.DatabaseLogID == id).First();
+            item.PostTime = DateTime.Parse(textPostTime.Text);
+            item.Event = textEvent.Text;
+            db.SaveChanges();
+            //Reset the edit index.
+            GridView1.EditIndex = -1;
+
+            //Bind data to the GridView control.
+            BindData();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            //Set the edit index.
+            GridView1.EditIndex = e.NewEditIndex;
+            //Bind data to the GridView control.
+            BindData();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            //Reset the edit index.
+            GridView1.EditIndex = -1;
+            //Bind data to the GridView control.
+            BindData();
+        }
+
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+            var db = new AdventureWorks2019Entities();
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            int id = int.Parse(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            var item = db.DatabaseLog.Where(x => x.DatabaseLogID == id).First();
+            db.DatabaseLog.Remove(item);
+            db.SaveChanges();
+
+            BindData();
+        }
+
+
+        private void BindData()
+        {
+            var db = new AdventureWorks2019Entities();
+            var dbLog = from log in db.DatabaseLog select log;
+            if (dbLog != null)
+            {
+                GridView1.DataSource = dbLog.ToList();
+                GridView1.DataBind();
+            }
+        }
+
+        protected void OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+
+            BindData();
+        }
+    }
+}
